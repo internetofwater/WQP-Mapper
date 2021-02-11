b424e5175557f59fa5f45371144dcf79332832cc
Your Name <you@example.com>
2021-02-10 19:17:56 -0500

    Updated dataretrieval and plots for pull from WQP instead of NWIS

--- Archive/DataRetrievalArchive.R
+++ Archive/DataRetrievalArchive.R
@@ -1,19 +1,15 @@
-# Data Retrieval Script 
-# Rachel Landman 
-# Spring 2021
+# SetUp 
 
-# Load Packages 
 library(dataRetrieval)
 library(sf)
-library(mapview)
+library(dplyr)
+library(ggplot2)
+library(ggmap)
 
-getwd()
-
-### GET WATERSHED BOUNDARY FROM NLDI #####
 # Step 1: Pick point
 # 36.0356035,-78.9001728
 
-## Ellerbe Creek HUC8
+## Ellerbe HUC8
 # 03020201
 
 # Step 2: Pass to NLDI
@@ -31,58 +27,64 @@ point_nldi_basin_get <- function(lat = y, lon = x){
   return(basin)
 }
 b <- point_nldi_basin_get(lat = y, lon = x)
-
-# Step 3: Convert watershed to a bbox to feed into WQP 
 bbox <- sf::st_bbox(b)
 bbox
+mapview::mapview(b)
 
-# Step 4: Identify WQP sites within the watershed 
+# Identify NWIS and WQP sites within the watershed 
+### They are the same
+
+NWISsites <- whatNWISsites (bBox = c(-78.98523, 36.01312, -78.88892, 36.04652))
+
 WQPsites <- whatWQPsites (bBox = c(-78.98523, 36.01312, -78.88892, 36.04652))
 
-write.csv(WQPsites, file = "WQPsites.csv")
+# Identify Data Available
+dataAvailable <- whatNWISdata(bBox = c(-78.98523, 36.01312, -78.88892, 36.04652))
+# Parameter Codes
+dataAvailable$parm_cd
 
-# Step 5: whatWQPdata - This returns a data frame with all of the sites that were measured. 
+# WQP
+# whatWQPdata - This returns a data frame with all of the sites that were measured. 
 # Also, in that table, there is a measure of activityCount (how often the site was sampled)
 # resultCount (how many individual results are available).
 dataAvailableWQP <- whatWQPdata(bBox = c(-78.98523, 36.01312, -78.88892, 36.04652))
 
-write.csv(dataAvailableWQP, file = "WQPdataavailable.csv")
-
-# Step 6: readWQPdata - all the data available from the WQP within the bounding box
-# data from the WQP using generalized Web service calls
+# readWQPdata - data from the WQP using generalized Web service calls
 WQPData <- readWQPdata(bBox = c(-78.98523, 36.01312, -78.88892, 36.04652))
-
-# List of characteristics and organizations 
-Characteristics <- unique(WQPData[c("CharacteristicName")])
-Organizations <- unique(WQPData[c("OrganizationFormalName")])
+WQPData$CharacteristicName
 
 
-# what samples 
-# The function whatWQPsamples returns information on the individual samples collected at a site
+# what samples - The function whatWQPsamples returns information on the individual samples collected at a site
 WQPSamples <- whatWQPsamples(bBox = c(-78.98523, 36.01312, -78.88892, 36.04652))
 
-#Get URL for dataset
+WQPmetrics <- whatWQPmetrics(bBox = c(-78.98523, 36.01312, -78.88892, 36.04652))
+
+#Get URL
 attr(WQPData, "url")
 siteInfo <- attr(WQPData, "siteInfo")
 
-# Step 7: Plot Data 
+##############################################################################
+################################    Test 1    ################################
+##############################################################################
 
-# Coord System for WQP data is NAD83 EPSG:4269
+#Plot Data 
+# Coord System NAD83 EPSG:4269
 data_tabular <-
-  as.data.frame(dataAvailableWQP) # use this to drop the tibble classes (i.e., tbl_df and tbl)
+  as.data.frame(dataAvailable) # use this to drop the tibble classes (i.e., tbl_df and tbl)
 
 # look at class
 class(data_tabular)
 
 # use names() to get spatial data columns
 names(data_tabular)
-# lat is y value and lon is x value
+# LAT is y value and LNG is x value
 
 # convert to sf object
-data_tabular_as_sf <- st_as_sf(data_tabular, coords = c("lon", "lat"), crs = 4269, dim = "XY") # lat long so set CRS = NAD83
+data_tabular_as_sf <- st_as_sf(data_tabular, coords = c("dec_long_va", "dec_lat_va"), crs = 4269, dim = "XY") # lat long so set CRS = NAD83
 
 # check class and CRS
 class(data_tabular_as_sf)
+head(data_tabular_as_sf)
 st_crs(data_tabular_as_sf)
 
 #Change crs
@@ -96,13 +98,32 @@ st_crs(data_tabular_as_sf_wgs84)
 ec_data <- data_tabular_as_sf_wgs84 %>%
   st_intersection(b)
 
-# Plot with Mapview 
-# Sites within the bounding box
-mapview::mapview(data_tabular_as_sf_wgs84, zcol = "activityCount")
 
-# Sites within the watershed 
+mapview::mapview(b)
+
+mapview::mapview(data_tabular_as_sf_wgs84)
+
+mapview::mapview(ec_data, zcol = "parm_cd", legend = TRUE)
+
+mapview::mapview(ec_data)
+
+
+### Read site info ###
+siteNumbers <- ec_data$site_no
+siteINFO <- readNWISsite(siteNumbers)
+siteINFO
+
+### Read site info ###
+parameterCd <- ec_data$parm_cd
+parameterINFO <- readNWISpCode(parameterCd)
+
 mapview::mapview(list("Watershed"=b, "Sites"=ec_data), 
-                 col.regions=c("blue", "red"), 
-                 homebutton = mapviewGetOption("homebutton"), cex=5,
-                 zcol = "activityCount")
+                 col.regions=c("red", "blue"))
+
+
+
+
+
+
+
 
